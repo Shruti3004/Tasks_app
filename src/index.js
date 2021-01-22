02d136bd3c9019ae191fa7e26e899943f577a963
Shruti3004 <mail2shruti.ag@gmail.com>
2021-01-22 17:42:03 +0530

    Created read end point for users

--- src/index.js
+++ src/index.js
@@ -20,6 +20,30 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req,res)=> {
+    // This is gonna return all the users
+    User.find({}).then(users => {
+        res.send(users);
+    }).catch(error => {
+        res.status(500).send();
+    })
+})
+
+app.get('/users/:id', (req, res)=> {
+    console.log(req.params);
+    const _id = req.params.id;
+    // Mongoose automatically converts string id to Object id
+    User.findById(_id).then(user => {
+        if(!user){
+            // Ye tab aayega jab hexadecimal hi id hogi lekin User mil nahi raha hai
+            return res.status(404).send("User NOt found");
+        }
+        res.send(user);
+    }).catch(error => {
+        res.status(500).send(`Internal Server Error: ${error}`);
+    })
+})
+
 app.post("/tasks", (req,res) => {
     const task = new Tasks(req.body);
     task.save()
