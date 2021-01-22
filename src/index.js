6259ee4e61ceb97716c18056ae55df7f63bdf982
Shruti3004 <mail2shruti.ag@gmail.com>
2021-01-22 16:46:23 +0530

    Created end point for tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user')
+const User = require('./models/user');
+const Tasks = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,10 +13,20 @@ app.post('/users', (req, res) => {
     const user = new User(req.body);
     user.save()
     .then(user => {
-        res.send(user);
+        res.status(201).send(user);
     }).catch(error => {
         res.status(400).send(error);
         // res.send(`Error: ${error}`);
+    })
+})
+
+app.post("/tasks", (req,res) => {
+    const task = new Tasks(req.body);
+    task.save()
+    .then(task => {
+        res.status(201).send(task);
+    }).catch(error => {
+        res.status(400).send(error);
     })
 })
 
