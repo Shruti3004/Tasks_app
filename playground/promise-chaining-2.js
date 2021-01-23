deddd53e7b934982c9224bc4a2976aeea25209ea
Shruti3004 <mail2shruti.ag@gmail.com>
2021-01-23 06:25:06 +0530

    Async await with mongoose

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,22 @@
 require('../src/db/mongoose');
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("600a2f09816e6525eebb0c88").then(task => {
-    console.log(task);
-    return Task.countDocuments({completed: false})
-}).then(result => {
-    console.log(result);
+// Task.findByIdAndDelete("600a2f09816e6525eebb0c88").then(task => {
+//     console.log(task);
+//     return Task.countDocuments({completed: false})
+// }).then(result => {
+//     console.log(result);
+// }).catch(e => {
+//     console.log(e);
+// })
+
+const DeleteTaskAndCount = async (id, completed) => {
+    const deleteTask = await Task.findByIdAndDelete(id);
+    return count = await Task.countDocuments({completed});
+}
+
+DeleteTaskAndCount("600a24a83b07241ecaeae9b9", false).then(count => {
+    console.log(count);
 }).catch(e => {
     console.log(e);
 })
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate("600a2d50394a3e24a0c8d704", { age: 1}).then(user => {
-    console.log(user);
-    return User.countDocuments({age: 1});
-}).then(result => {
-    console.log(result);
+// User.findByIdAndUpdate("600a2d50394a3e24a0c8d704", { age: 1}).then(user => {
+//     console.log(user);
+//     return User.countDocuments({age: 1});
+// }).then(result => {
+//     console.log(result);
+// }).catch(e => {
+//     console.log(e);
+// });
+
+const UpdateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age});
+    const count = await User.countDocuments({age});
+    return count;
+}
+
+UpdateAgeAndCount('600a2d50394a3e24a0c8d704', 3).then(count => {
+    console.log(count);
 }).catch(e => {
     console.log(e);
 })
