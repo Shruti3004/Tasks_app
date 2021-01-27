b517d0bb05f7d0b260682010eb84547d0a04508e
Shruti3004 <mail2shruti.ag@gmail.com>
2021-01-28 02:40:01 +0530

    Generating Auth Token

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -40,10 +41,25 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive no');
             }
         }
-    }
+    },
+    tokens : [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
+// Instance message
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({_id: user.id.toString()}, 'thisismynewcourse');
+    user.tokens = user.tokens.concat({token});
+    await user.save()
+    return token;
+}
 
+// Model message
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({email});
     if(!user){
--- src/routers/user.js
+++ src/routers/user.js
@@ -17,7 +17,8 @@ router.post('/users', async (req, res) => {
     // })
     try{
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token });
     } catch (error){
         res.status(400).send(error);
     }
@@ -27,8 +28,9 @@ router.post('/users', async (req, res) => {
 
 router.post('/users/login', async (req, res)=> {
     try{
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user);
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken()
+        res.send({ user, token });
     }catch(e){
         res.status(400).send(e);
     }
