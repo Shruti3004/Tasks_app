568dc069ffeee363102825bbea167599974e9af1
Shruti3004 <mail2shruti.ag@gmail.com>
2021-01-30 00:51:13 +0530

    Validating file uploads for the user-avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -6,9 +6,7 @@ const User = require("../models/user");
 const auth = require('../middleware/auth')
 require('../db/mongoose');
 const multer = require('multer');
-const upload = multer({
-    dest: 'avatars'
-})
+
 
 router.post('/users', async (req, res) => {
     
@@ -96,6 +94,18 @@ router.get('/users/me', auth ,async (req,res)=> {
 
 
 
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('File must be jpg, jpeg or png'));
+        }
+        cb(undefined, true);
+    }
+})
 router.post('/users/me/avatar', upload.single('avatar'), (req,res) => {
     res.send();
 })
